b597984af20cb3cefac4fd36eb6c087b9bb4f138
candidoaj <candidoaj@gmail.com>
2020-02-19 17:25:35 -0300

    Criando Dao produto

---
+++ Alura.Loja.Testes.ConsoleApp/ProdutoDaoEntity.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alura.Loja.Testes.ConsoleApp
+{
+    public class ProdutoDaoEntity : IProdutoDao
+    {
+        public void Adicionar(Produto produto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Atualizar(Produto produto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<Produto> Produtos()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remover(Produto produto)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
